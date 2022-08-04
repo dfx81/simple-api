71e828f6e2e1f148b891a7a7138a859bd42ed506
Danial Fitri <48023732+dfx81@users.noreply.github.com>
2022-08-04 17:10:57 +0800

    Edited some endpoint routes documentation

--- api/routes/index.py
+++ api/routes/index.py
@@ -7,15 +7,15 @@ index = Blueprint("index", __name__)
 def help():
 	message = (
 		"<h1>Simple API created by dfx</h1>"
-		"<h2>Endpoints</h2>"
+		"<hr/><h2>Endpoints</h2>"
 		"<ul>"
 		"<li><b>[GET]</b> <code>/</code> : View this page</li>"
 		"<li><b>[GET]</b> <code>/ping</code> : Ping the API</li>"
-		"<li><b>[GET]</b> <code>/link</code> : Get list of links to dfx's socials</li>"
+		"<li><b>[GET]</b> <code>/links</code> : Get list of links to dfx's socials</li>"
 		"<li><b>[GET]</b> <code>/roulette</code> : Get a random Vocaloid/UTAU song curated by dfx</li>"
 		"<li><b>[GET]</b> <code>/roulette/&lt;playlist-id&gt;</code> : Create your own roulette</li>"
 		"<li><b>[GET]</b> <code>/list/&lt;playlist-id&gt;</code> : Get list of items in playlist</li>"
-		"</ul>"
+		"</ul><hr/>"
 		"<small>&copy; 2022, QLYCO / dfx</small>"
 	)
 
